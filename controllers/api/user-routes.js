ca7ef347aa046b111827ccedc2a5939b522d20c6
Zach Smith <Lemelisk27@gmail.com>
2021-11-03 18:24:42 -0700

    added profile page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {User,Survey} = require("../../models")
-const sequelize = require('../../config/connection');
+const sequelize = require('../../config/connection')
+const bcrypt = require("bcrypt")
 
 router.get("/",(req,res)=>{
     User.findAll({
@@ -12,6 +13,33 @@ router.get("/",(req,res)=>{
     }).catch(err=>{
         console.log(err)
         res.status(500).json({message:"An Error Occured",err:err})
+    })
+})
+
+router.post("/login",(req,res)=>{
+    User.findOne({
+        where:{
+            username:req.body.username
+        }
+    }).then(foundUser=>{
+        if(!foundUser){
+            req.session.destroy()
+            res.redirect("/")
+        }
+        else {
+            if(bcrypt.compareSync(req.body.password,foundUser.password)){
+                req.session.user = {
+                    username:foundUser.username,
+                    email:foundUser.email,
+                    id:foundUser.id
+                }
+                res.json(foundUser)
+            }
+            else{
+                req.session.destroy
+                res.redirect("/")
+            }
+        }
     })
 })
 
--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const sequelize = require('../config/connection');
+const {User,Survey} = require("../models")
 
 router.get("/",(req,res)=>{
     let landingpage = true
     res.render("landingpage",{
         landingpage:landingpage
+    })
+})
+
+router.get("/profile",(req,res)=>{
+    if(!req.session.user){
+        res.redirect("/")
+        return
+    }
+    User.findOne({
+        where: {
+            id:req.session.user.id
+        },
+        include: [Survey],
+        attributes:{
+            include: [
+                [sequelize.fn('date_format', sequelize.col('birthdate'), '%m-%d-%Y'), 'birthdate']
+            ]
+        }
+    }).then(userData=>{
+        const hbsUser = userData.get({plain:true})
+        res.render("profile",{
+            user:hbsUser
+        })
     })
 })
 
